54e9a65b494b2b39217c26ef92f87ef2df60444e
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-05-29 15:24:19 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { Client } from "@hubspot/api-client";
 
 const App = () => {
+  const hubspotClient = new Client({
+    accessToken: "pat-na1-59d2b73e-340a-4a01-900c-fd62cc975dc6",
+  });
+
+  const allContacts = hubspotClient.crm.contacts.getAll();
+
+  allContacts
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return <div>App React</div>;
 };
 
