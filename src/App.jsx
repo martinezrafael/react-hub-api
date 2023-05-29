5a8c37dc9c04796095bc5fb215fbd597b9cd57a9
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-05-29 15:25:50 -0300

    alterando o basepath

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Client } from "@hubspot/api-client";
 const App = () => {
   const hubspotClient = new Client({
     accessToken: "pat-na1-59d2b73e-340a-4a01-900c-fd62cc975dc6",
+    basePath: "https://react-hub-api.vercel.app/",
   });
 
   const allContacts = hubspotClient.crm.contacts.getAll();
